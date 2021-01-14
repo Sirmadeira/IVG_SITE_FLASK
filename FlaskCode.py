1fe11b611a5acd0880d00e9dc19068c6ceb74fba
Sirmadeira <nicolaumori@gmail.com>
2021-01-14 16:38:43 -0300

    UHU terminei

--- FlaskCode.py
+++ FlaskCode.py
@@ -1,16 +1,28 @@
 from flask import Flask, render_template , url_for
-from formularios import FormularioDeRegistro, FormularioDeLogin
+from form import FormularioDeRegistro, FormularioDeLogin
+
+
 app=Flask(__name__)
-app.config['CHAVE_SEGREDO'] = 'e4e0a64a5879d4ce983fa8d1b24ed354' 
+app.config['SECRET_KEY'] = 'a3c293dd89c6703bfa4836e4059e3cdf'
 
 @app.route("/")
+@app.route("/HomePage")
+def HomePage():
+	return render_template("HomePage.html",title = "HomePage")
+
+@app.route("/Cadastro")
+def Cadastro():
+	form = FormularioDeRegistro()
+	return render_template('Cadastro.html',title = "Cadastro", form = form)
+
 @app.route("/Login")
 def Login():
-	return render_template("Login.html")
+	form= FormularioDeLogin()
+	return render_template("Login.html",title= "Login", form = form)
 
 @app.route("/Sobre")
 def Sobre():
-	return render_template("Sobre.html", title="Sobre")
+	return render_template("Sobre.html", title = "Sobre")
 
 @app.route("/SegundaJanela")
 def SegundaJanela():
@@ -18,15 +30,11 @@ def SegundaJanela():
 	
 @app.route("/TerceiraJanela")
 def TerceiraJanela():
-	return render_template("TerceiraJanela.html", title= "TerceiraJanela")
-
-@app.route("/HomePage")
-def HomePage():
-	return render_template("HomePage.html")
+	return render_template("TerceiraJanela.html", title = "TerceiraJanela")
 
 @app.route("/Contato")
 def Contato():
-	return render_template("Contato.html")
+	return render_template("Contato.html", title = "Contato")
 
 if __name__ == "__main__":
 	app.run(debug=True)
