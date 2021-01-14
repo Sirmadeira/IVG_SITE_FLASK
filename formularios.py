cca529390ce5f1a4c9a46418a26332dec9e84264
Sirmadeira <nicolaumori@gmail.com>
2021-01-14 11:53:09 -0300

    helo

--- formularios.py
+++ formularios.py
@@ -1,13 +1,14 @@
 from flask_wtf	import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo	
+from wtforms.validators import DataRequired, Length, EqualTo ,Email
+
 
 class FormularioDeRegistro(FlaskForm):
 	Usuario =StringField('Usuario', 
 						validators = [DataRequired(),Length(min= 2, max=20)]) 
 
 	Email = StringField('Email',
-						validators=[DataRequired(),Email()])
+						validators=[DataRequired(), Email()])
 
 	Senha= PasswordField('Senha',
 						validators=[DataRequired(),Length(min=5,max=20)])
@@ -22,10 +23,11 @@ class FormularioDeLogin(FlaskForm):
 						validators = [DataRequired(),Length(min= 2, max=20)]) 
 
 	Email = StringField('Email',
-						validators=[DataRequired(),Email()])
+						validators=[DataRequired(), Email()])
 
 	Lembrete= BooleanField("Lembre-se de mim")
 
 	Senha= PasswordField('Senha',
 						validators=[DataRequired(),Length(min=5,max=20)])
+
 	Confirma=SubmitField('Entre')
