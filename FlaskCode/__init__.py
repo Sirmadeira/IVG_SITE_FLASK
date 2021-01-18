ee252ad12b722846b605edbdfa5fe708442881d7
Sirmadeira <nicolaumori@gmail.com>
2021-01-18 14:40:44 -0300

    aaa

--- FlaskCode/__init__.py
+++ FlaskCode/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
 
 app=Flask(__name__)
 app.config['SECRET_KEY'] = 'a3c293dd89c6703bfa4836e4059e3cdf'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///UsuarioIVG.db'
 db= SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 
-from FlaskCode import Rotas
+from __init__ import Rotas
