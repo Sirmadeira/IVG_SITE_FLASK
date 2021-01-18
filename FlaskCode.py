efe087f915acea8e679d785ef193bc208da84c27
Sirmadeira <nicolaumori@gmail.com>
2021-01-18 11:48:01 -0300

    Adicao de base de dados de usuario

--- FlaskCode.py
+++ FlaskCode.py
@@ -1,9 +1,25 @@
 from flask import Flask, render_template , url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from form import FormularioDeRegistro, FormularioDeLogin
+
 
 
 app=Flask(__name__)
 app.config['SECRET_KEY'] = 'a3c293dd89c6703bfa4836e4059e3cdf'
+app.config['SQLAlCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db= SQLAlchemy(app)
+
+class UsuariosDB(db.Model):
+	id = db.Column(db.Integer, primary_key= True)
+	UsuarioDB = db.Column(db.String(20), unique= True, nullable = False)
+	EmailDB = db.Column(db.String(120), unique= True, nullable = False)
+	SenhaDB = db.Column(db.String(60), nullable = False)
+
+
+	def __repr__(self):
+		return f"UsuariosDB('{self.UsuarioDB}','{self.EmailDB}')"
+
+		
 @app.route("/")
 @app.route("/HomePage")
 def HomePage():
