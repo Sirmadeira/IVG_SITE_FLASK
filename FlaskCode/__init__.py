27ba8651f18706dc81b2ec3ae7539cce76342c33
Sirmadeira <nicolaumori@gmail.com>
2021-01-18 18:33:45 -0300

    Revert "Revert "Criando Usuarios""
    This reverts commit 00b5548ae8c5e43068314af130712fa155275ff5.

---
+++ FlaskCode/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+app=Flask(__name__)
+app.config['SECRET_KEY'] = 'a3c293dd89c6703bfa4836e4059e3cdf'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///UsuarioIVG.db'
+db= SQLAlchemy(app)
+bcrypt = Bcrypt(app)
