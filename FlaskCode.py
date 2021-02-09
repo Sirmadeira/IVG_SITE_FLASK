254e847fa43215179e9537612764b4cbcbf51837
Sirmadeira <71156378+Sirmadeira@users.noreply.github.com>
2021-02-09 16:13:44 -0300

    Update FlaskCode.py

--- FlaskCode.py
+++ FlaskCode.py
@@ -160,13 +160,23 @@ class ResetSenha(FlaskForm):
 
 class DadosEssenciais(FlaskForm):
 
+     MarcasGarantia = ("Acura", "Agrale","Alfo Romeo","Am Gen","Asia motors","ASTON MARTIN","Audi","Baby","BMW",
+    "BRM","BUGRE","Cadillac","CBT Jipe","CHANA","CHANGAN","CHERY","Chrysler","Citroën",
+    "Cross Lander","Daewoo","Daihatsu","Dodge","EFFA","Engesa","Envemo","Ferrari","Fiat",
+    "Fibravan","Ford","FOTON","Fyber","GEELY","GM CHEVROLET","GREAT WALL","Gurgel","HAFEI",
+    "HITECH ELECTRIC","HONDA","HYUNDAY","ISUZU","IVECO","JAC","Jaguar","Jeep","JINBEI","JPX",
+    "Kia Motors","Lada","Lamborghini","Land Rover","Lexus","LIFAN","LOBINI","Lotus","Mahindra",
+    "Maserati","Matra","Mazda","Mclaren","Mercedez-Benz","Mercury","MG","MINI","Mitsubishi","Miura",
+    "Nissan","Peugeot","Plymouth","Pontiac","Porsche","RAM","RELY","Renault","Rolls-Royce","Rover",
+    "Saab","Saturn","Seat","SHINERAY","smart","SSANGYONG","Subaru","Suzuki","TAC","Toyota","Troller","Volvo","VW-VOLKSWAGEN","Wake","Walk")
+
     Localidades= ("Limeira", "Piracicaba")
 
     CoresGarantia = ("Amarelo","Azul","Bege","Branco","Bronze","Cinza","Dourado","Indefinida","Laranja","Marrom","Prata","Preto",
                     "Rosa","Roxo","Verde","Vermelho","Vinho")
     
     Marca = StringField('Marca',id= "marca_autocomplete:",
-                        validators=[InputRequired(message='Favor inserir uma Marca valida')])
+                        validators=[InputRequired(message='Favor inserir uma Marca valida'), AnyOf(MarcasGarantia, message= MarcasGarantia)])
     
     Modelo = StringField('Modelo',
                         validators=[InputRequired(message='Favor inserir um modelo valido')])
