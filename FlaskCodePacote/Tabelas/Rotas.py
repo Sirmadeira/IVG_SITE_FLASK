6928907759bcc85b26bcdd8438fac3a184a7bb46
Sirmadeira <71156378+Sirmadeira@users.noreply.github.com>
2021-03-04 13:23:20 -0300

    Tabela margem de lucro

--- FlaskCodePacote/Tabelas/Rotas.py
+++ FlaskCodePacote/Tabelas/Rotas.py
@@ -29,16 +29,19 @@ def SegundaJanela():
 @login_required
 def TerceiraJanela():
     TituloDadosInserido = ("Marca","Modelo",'Motor','Combustivel', "Ano", "Quilometragem" , "Preço" , "Cor" , "Margem de Lucro" ,"Controle" )
-    TituloTop20Modelo=("Marca","Modelo","n.º de vendas")
+    TituloTop20Modelo=("Marca","Modelo","N.º de vendas")
     TituloTop20Marca=("Marca"," N.º de vendas")
+    TituloMargemLucro=('Modelo','Média de Lucro')
     contador= Dado.query.filter_by(NomeDaEmitente = current_user.NomeDaEmpresaDB).count()
     faltante= 5-contador
     if contador is None or contador < 5:
         return render_template("TerceiraJanelaSemDados.html", title = "TerceiraJanela",contador= contador, faltante= faltante)
     else:
-        return render_template("TerceiraJanela.html", title = "TerceiraJanela", TituloDadosInserido =TituloDadosInserido,Query=Dado.query.filter_by(NomeDaEmitente = current_user.NomeDaEmpresaDB).order_by(Dado.id.desc()).limit(10).all(),
+        return render_template("TerceiraJanela.html", title = "TerceiraJanela", 
+                                TituloDadosInserido =TituloDadosInserido,Query=Dado.query.filter_by(NomeDaEmitente = current_user.NomeDaEmpresaDB).order_by(Dado.id.desc()).limit(10).all(),
                                 TituloTop20Modelo= TituloTop20Modelo, ContadorGrupo= Dado.query.with_entities(Dado.MarcaDB,Dado.ModeloDB,func.count(Dado.ModeloDB).label('total')).group_by(Dado.ModeloDB).order_by(desc('total')).limit(20),
-                                TituloTop20Marca=TituloTop20Marca, ContadorGrupo2= Dado.query.with_entities(Dado.MarcaDB,func.count(Dado.MarcaDB).label('total2')).group_by(Dado.MarcaDB).order_by(desc('total2')).limit(20).all())
+                                TituloTop20Marca=TituloTop20Marca, ContadorGrupo2= Dado.query.with_entities(Dado.MarcaDB,func.count(Dado.MarcaDB).label('total2')).group_by(Dado.MarcaDB).order_by(desc('total2')).limit(20).all(),
+                                TituloMargemLucro= TituloMargemLucro, ContadorGrupo3= Dado.query.with_entities(Dado.MarcaDB,Dado.ModeloDB,func.avg(Dado.MargemDeLucroDB).label('media')).group_by(Dado.ModeloDB).order_by(desc('media')).limit(40).all())
 
     return render_template("TerceiraJanelaSemDados.html", title = "TerceiraJanela") 
 
