dcf78bcecb69dcbf9b44c5a9f0e595826468260c
Sirmadeira <71156378+Sirmadeira@users.noreply.github.com>
2021-03-13 15:18:59 -0300

    Fodac

--- FlaskCodePacote/Principal/Utilidades.py
+++ FlaskCodePacote/Principal/Utilidades.py
@@ -2,7 +2,7 @@ from flask_mail import Message
 from FlaskCodePacote import mail, db
 from FlaskCodePacote.Modelos import Dado, UsuarioDB
 from sqlalchemy import desc
-from datetime import datetime
+import datetime
 
 def enviar_email_checkup():
 	users= [1,2]
@@ -11,7 +11,7 @@ def enviar_email_checkup():
 		dueduser=db.session.query(UsuarioDB.EmailDB,UsuarioDB.id).filter_by(id = user).order_by(UsuarioDB.id.desc()).limit(1).scalar()
 		datahoje=datetime.datetime.utcnow()
 		diff= datahoje-ultimadata
-		if diff.days >= 2:
+		if diff.days >= 14:
 			msg = Message('Falta de dados',sender='ivgnoreply@gmail.com', recipients= [dueduser])
 			msg.body =   'Isso e um email para vc parar de ser vagabundo'
 			mail.send(msg)
