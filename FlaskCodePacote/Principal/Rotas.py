7f7b2a8840bc8fac15543bd1184635de26bafd60
Sirmadeira <71156378+Sirmadeira@users.noreply.github.com>
2021-03-15 11:27:17 -0300

    d

--- FlaskCodePacote/Principal/Rotas.py
+++ FlaskCodePacote/Principal/Rotas.py
@@ -8,7 +8,6 @@ Principal = Blueprint('Principal', __name__)
 @Principal.route("/HomePage", methods=['GET', 'POST'])
 def HomePage():
 	return render_template("HomePage.html",title = "HomePage")
-
 @Principal.route("/Sobre")
 def Sobre():
     return render_template("Sobre.html", title = "Sobre")
