6497333ea87347689ec1e02e61824328320d7e47
Sirmadeira <71156378+Sirmadeira@users.noreply.github.com>
2021-03-25 13:57:17 -0300

    Melhorando restfull e acrescentando anos

--- FlaskCodePacote/Tabelas/Rotas.py
+++ FlaskCodePacote/Tabelas/Rotas.py
@@ -30,19 +30,23 @@ def SegundaJanela():
 def TerceiraJanela():
     PresentePython = '%'
     TituloDadosInserido = ("Marca","Modelo",'Motor','Combustivel', "Ano", "Quilometragem" , "Preço" , "Cor" , "Margem de Lucro" ,"Controle" )
-    TituloTop20Modelo=("Marca","Modelo","N.º de vendas")
+    TituloTop20Modelo=("Marca","Modelo","Ano","N.º de vendas")
     TituloTop20Marca=("Marca"," N.º de vendas")
-    TituloMargemLucro=('Marca','Modelo','Média de Lucro')
+    TituloMargemLucro=('Marca','Modelo',"Ano",'Média de Lucro')
     contador= Dado.query.filter_by(NomeDaEmitente = current_user.NomeDaEmpresaDB).count()
+    Query=Dado.query.filter_by(NomeDaEmitente = current_user.NomeDaEmpresaDB).order_by(Dado.id.desc()).limit(10).all()
+    ContadorGrupo= Dado.query.with_entities(Dado.MarcaDB,Dado.ModeloDB,Dado.AnoDB,func.count(Dado.ModeloDB).label('total')).group_by(Dado.ModeloDB).order_by(desc('total')).limit(20)
+    ContadorGrupo2= Dado.query.with_entities(Dado.MarcaDB,func.count(Dado.MarcaDB).label('total2')).group_by(Dado.MarcaDB).order_by(desc('total2')).limit(20).all()
+    ContadorGrupo3= Dado.query.with_entities(Dado.MarcaDB,Dado.ModeloDB,Dado.AnoDB,func.avg(Dado.MargemDeLucroDB).label('media')).group_by(Dado.ModeloDB).order_by(desc('media')).all()
     faltante= 5-contador
     if contador is None or contador < 5:
         return render_template("TerceiraJanelaSemDados.html", title = "TerceiraJanela",contador= contador, faltante= faltante)
     else:
         return render_template("TerceiraJanela.html", title = "TerceiraJanela",PresentePython= PresentePython, 
-                                TituloDadosInserido =TituloDadosInserido,Query=Dado.query.filter_by(NomeDaEmitente = current_user.NomeDaEmpresaDB).order_by(Dado.id.desc()).limit(10).all(),
-                                TituloTop20Modelo= TituloTop20Modelo, ContadorGrupo= Dado.query.with_entities(Dado.MarcaDB,Dado.ModeloDB,func.count(Dado.ModeloDB).label('total')).group_by(Dado.ModeloDB).order_by(desc('total')).limit(20),
-                                TituloTop20Marca=TituloTop20Marca, ContadorGrupo2= Dado.query.with_entities(Dado.MarcaDB,func.count(Dado.MarcaDB).label('total2')).group_by(Dado.MarcaDB).order_by(desc('total2')).limit(20).all(),
-                                TituloMargemLucro= TituloMargemLucro, ContadorGrupo3= Dado.query.with_entities(Dado.MarcaDB,Dado.ModeloDB,func.avg(Dado.MargemDeLucroDB).label('media')).group_by(Dado.ModeloDB).order_by(desc('media')).all())
+                                TituloDadosInserido =TituloDadosInserido,Query=Query,
+                                TituloTop20Modelo= TituloTop20Modelo, ContadorGrupo= ContadorGrupo,
+                                TituloTop20Marca=TituloTop20Marca, ContadorGrupo2= ContadorGrupo2,
+                                TituloMargemLucro= TituloMargemLucro, ContadorGrupo3= ContadorGrupo3)
 
     return render_template("TerceiraJanelaSemDados.html", title = "TerceiraJanela") 
 
