8bfc9263f9e0e4652808ecddb461454ff926a956
Sirmadeira <71156378+Sirmadeira@users.noreply.github.com>
2021-03-31 14:14:29 -0300

    nada dms

--- FlaskCodePacote/__init__.py
+++ FlaskCodePacote/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
-from FlaskCodePacote.Config import Config
+from FlaskCodePacote.config import Config
 
  
 db = SQLAlchemy()
