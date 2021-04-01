f0edc8f4dec983b92a6dc7a508ebfb8da7132ded
Sirmadeira <71156378+Sirmadeira@users.noreply.github.com>
2021-04-01 15:11:21 -0300

    a

--- FlaskCodePacote/Config.py
+++ FlaskCodePacote/Config.py
@@ -4,11 +4,10 @@ class Config:
 
 	SECRET_KEY = os.environ.get('SECRET_KEY')
 	SQLALCHEMY_DATABASE_URI= os.environ.get('SQLALCHEMY_DATABASE_URI')
-	MAIL_SERVER = 'smtp.googlemail.com'
-	MAIL_PORT = 587
-	MAIL_USE_TLS = True
+	MAIL_SERVER = 'smtp.gmail.com'
+	MAIL_PORT = 465
+	MAIL_USE_SSL = True
 	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
 	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-	MAIL_MAX_EMAILS = 2
 	RECAPTCHA_PUBLIC_KEY = os.environ.get('RECAPTCHA_PUBLIC_KEY')
 	RECAPTCHA_PRIVATE_KEY = os.environ.get('RECAPTCHA_PRIVATE_KEY')
